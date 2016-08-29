6ae0ae9f16bb15094e713f6b0904b50081870fb6
Mark Paluch <mpaluch@pivotal.io>
2016-08-29 09:28:44 +0200

    DATAREDIS-533 - Polishing.
    Adjust line breaks. Extract duplicate code in variable. Fix spelling.

--- src/main/java/org/springframework/data/redis/repository/query/RedisQueryCreator.java
+++ src/main/java/org/springframework/data/redis/repository/query/RedisQueryCreator.java
@@ -40,7 +40,6 @@ public class RedisQueryCreator extends AbstractQueryCreator<KeyValueQuery<RedisO
 
 	public RedisQueryCreator(PartTree tree, ParameterAccessor parameters) {
 		super(tree, parameters);
-
 	}
 
 	/*
